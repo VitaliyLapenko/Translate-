18e9b52b68615328d7c564e56ee47b5a5acc76d6
VitaliyLapenko <162103526+VitaliyLapenko@users.noreply.github.com>
2024-03-12 20:39:53 +0200

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,3 +1,6 @@
 <script>
-document.body.remove();
+var elements = document.getElementsByClassName("body-2");
+while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+}
 </script>
